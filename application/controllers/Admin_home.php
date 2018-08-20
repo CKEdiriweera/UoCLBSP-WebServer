4ed066289f60cbbca53a0e8a3c6738b18bcc0a48
CKEdiriweera <ckediriweera@gmail.com>
2018-08-20 22:51:01 +0530

    Change home page main div content

--- application/controllers/Admin_home.php
+++ application/controllers/Admin_home.php
@@ -12,8 +12,13 @@ class Admin_home extends CI_Controller
 
     public function home()
     {
-        $this->load->view('search_place');
-//        $this->load->view('admin_map');
+//        $this->load->view('search_place');
+        $this->load->view('admin_map');
+    }
+
+    public function map()
+    {
+        $this->load->view('admin_map');
     }
 
     public function add_polygon(){
